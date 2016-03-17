50a2aba19e195ae2de21543468d3bfc3f292b45b
Ali Fahmi <lab@advanced-network.org>
2016-03-17 19:29:54 +0700

    Update API Keys of BNI CC

--- src/biline/twitter/TwitterStreamBuilderUtil.java
+++ src/biline/twitter/TwitterStreamBuilderUtil.java
@@ -42,24 +42,18 @@ public class TwitterStreamBuilderUtil {
 			this.ACCESS_TOKEN		  = "69868781-v9VfpKq1n7j0nu2dWVOG9O0FS8Rl6CwqQW8NU7VA9";
 			this.ACCESS_TOKEN_SECRET  = "GcORBA5esOJ99kgPhUJyXhibj6qXhBzGjsdj521tqd2NO";
 		}
-		else if(twitterHandler.equals("bni46")){
-			this.CONSUMER_KEY		  = "zTHyBDaZzDBNpKQx98rNDWEtR";
-			this.CONSUMER_SECRET 	  = "mM2mwNfL2Vwga0DuteG9dDXuNOCNmPZzLZ1mZMlnKJPJkNf2TW";
-			this.ACCESS_TOKEN		  = "62455266-TbRhx8jNzSKNdU5eSKh9q4lnNCgtWSqdKXKNytCc2";
-			this.ACCESS_TOKEN_SECRET  = "aj5Ffh3xfvgBHtz3ahxE1T1M6jCiDCGNW7swo3WNgJV6x";
+		else if(twitterHandler.equals("bni46")){ //2nd Apps
+			this.CONSUMER_KEY		  = "mpmBSPcnCj0djPkIn4oQCtuvH";
+			this.CONSUMER_SECRET 	  = "QHVg6gTN9VqWPWJyknRwFdlHddqui4hSBYJzL1jQVSDPnIZS73";
+			this.ACCESS_TOKEN		  = "62455266-06Os7Be9lQD42tMfV3CwKXgD1LEhyKeoJzN0a8yWU";
+			this.ACCESS_TOKEN_SECRET  = "mWRVxhwh1ZPXtCSyrCL0cbor4UiBoTrw9YuHLksrcd34p";
 		}
-		//else if(twitterHandler.equals("bni46")){ //2nd Apps
-		//	this.CONSUMER_KEY		  = "mpmBSPcnCj0djPkIn4oQCtuvH";
-		//	this.CONSUMER_SECRET 	  = "QHVg6gTN9VqWPWJyknRwFdlHddqui4hSBYJzL1jQVSDPnIZS73";
-		//	this.ACCESS_TOKEN		  = "62455266-06Os7Be9lQD42tMfV3CwKXgD1LEhyKeoJzN0a8yWU";
-		//	this.ACCESS_TOKEN_SECRET  = "mWRVxhwh1ZPXtCSyrCL0cbor4UiBoTrw9YuHLksrcd34p";
-		//}
-		//else if(twitterHandler.equals("bni46_cs")){
-		//	this.CONSUMER_KEY		  = "n9BLTunGxilyRKFmdnVTKx6d3";
-		//	this.CONSUMER_SECRET 	  = "rVsQwwhDIMp2Qmaa0uvTKKOgzp3BgYH8fXjgYfcj8k8EQWM0im";
-		//	this.ACCESS_TOKEN		  = "3272444328-mD2oKkeDdb9wZd4yXhC9lrwHSkG249Rtmz0NXxb";
-		//	this.ACCESS_TOKEN_SECRET  = "7O78fyKyTy9V3ytZdFSSdFiVSTLgqEaBbISbzPTIXeL6n";
-		//}
+		else if(twitterHandler.equals("bni46_cs")){
+			this.CONSUMER_KEY		  = "2eAZTNPCwA4H4YAyy5gr76XKZ";
+			this.CONSUMER_SECRET 	  = "jvGCwWPjELtNdN8fnGhQ09LpU7e32uGdeSl7kAb4FjxwzaME0o";
+			this.ACCESS_TOKEN		  = "3272444328-8epr3Bfy8tXs5TS59sM3tbmjn9xYPc24deiqLT6";
+			this.ACCESS_TOKEN_SECRET  = "nO6mDSloPoeqW86A6UQwy7lJmoKji0ZpNmK5sqe0UVBvI";
+		}
 		else {
 			this.CONSUMER_KEY		  = "";
 			this.CONSUMER_SECRET 	  = "";
